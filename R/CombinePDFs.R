e26062a43d3a725d58a2a3ec47997a17958722b8
Jason C Fisher <jfisher@usgs.gov>
2014-01-24 16:46:48 -0700

    CombinePDFs accounts for number of pages in each PDF

--- R/CombinePDFs.R
+++ R/CombinePDFs.R
@@ -2,9 +2,6 @@
 # Requires PDFtk Server http://www.pdflabs.com/tools/pdftk-server/
 
 CombinePDFs <- function(path, open.pdf=FALSE) {
-  if (.Platform$OS.type != "windows")
-    stop("function requires a Windows platform")
-
   input.pdfs <- list.files(path, pattern=".pdf$")
   if (length(input.pdfs) == 0 || input.pdfs == "")
     stop("path does not exist or input pdf files are missing")
@@ -14,18 +11,25 @@ CombinePDFs <- function(path, open.pdf=FALSE) {
   if(file.exists(output.pdf))
     file.remove(output.pdf)
 
+  tmp.txt <- tempfile(pattern="tmp", tmpdir=tempdir(), fileext=".txt")
   tmp.pdf <- tempfile(pattern="tmp", tmpdir=tempdir(), fileext=".pdf")
-  cmd <- NULL
-  cmd[1] <- paste("CD /d", shQuote(path))
-  cmd[2] <- paste("pdftk", paste(shQuote(input.pdfs), collapse=" "),
-                  "cat output", shQuote(tmp.pdf))
   tmp.bat <- tempfile(pattern="tmp", tmpdir=tempdir(), fileext=".bat")
+  cmd <- paste("cd", shQuote(path))
+
+  npages <- NULL
+  for (i in input.pdfs) {
+    cmd[2] <- paste("pdftk", shQuote(i), "dump_data output", shQuote(tmp.txt))
+    cat(cmd, file=tmp.bat, sep="\n")
+    system(command=tmp.bat, show.output.on.console=FALSE)
+    txt <- scan(tmp.txt, what=character(), quiet=TRUE)
+    npages <- c(npages, as.integer(txt[which(txt == "NumberOfPages:") + 1L]))
+  }
+  pages <- cumsum(npages) - (npages - 1L)
+
+  cmd[2] <- paste("pdftk *.pdf cat output", shQuote(tmp.pdf))
   cat(cmd, file=tmp.bat, sep="\n")
   system(command=tmp.bat, show.output.on.console=FALSE)
 
-  tmp.txt <- tempfile(pattern="tmp", tmpdir=tempdir(), fileext=".txt")
-  cmd <- NULL
-  cmd[1] <- paste("CD /d", shQuote(path))
   cmd[2] <- paste("pdftk", shQuote(tmp.pdf), "dump_data output",
                   shQuote(tmp.txt))
   cat(cmd, file=tmp.bat, sep="\n")
@@ -34,20 +38,19 @@ CombinePDFs <- function(path, open.pdf=FALSE) {
   bookmarks <- sub("\\.pdf$", "", input.pdfs)
   Fun <- function(i) {
     return(paste("BookmarkBegin", paste("BookmarkTitle:", bookmarks[i]),
-                 "BookmarkLevel: 1", paste("BookmarkPageNumber:", i), sep="\n"))
+                 "BookmarkLevel: 1", paste("BookmarkPageNumber:", pages[i]),
+                                           sep="\n"))
   }
-  txt <- vapply(seq_along(bookmarks), Fun, "")
-  cat(txt, file=tmp.txt, sep="\n", append=TRUE)
-
-  cmd <- NULL
-  cmd[1] <- paste("CD /d", shQuote(path))
+  bookmarks <- vapply(seq_along(bookmarks), Fun, "")
+  cat(bookmarks, file=tmp.txt, sep="\n", append=TRUE)
   cmd[2] <- paste("pdftk", shQuote(tmp.pdf), "update_info", shQuote(tmp.txt),
                   "output", shQuote(output.pdf))
   cat(cmd, file=tmp.bat, sep="\n")
   system(command=tmp.bat, show.output.on.console=FALSE)
 
   if (open.pdf)
-    system(paste("open", shQuote(output.pdf)))
+    system(paste("open", shQuote(output.pdf)), wait=FALSE,
+           show.output.on.console=FALSE)
 
   invisible(output.pdf)
 }
