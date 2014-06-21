2a015fcdc17917b9d6a9ba232d772e5e2c00d273
Jason C. Fisher <jfisher@usgs.gov>
2014-06-21 10:56:42 -0600

    Minor changes

--- R/.TMP.R
+++ R/.TMP.R
@@ -1,7 +1,8 @@
 
 
 
-.ProcessRawData <- function(raw.data, parameters, detection.limits=NULL) {
+.ProcessRawData <- function(raw.data, parameters, detection.limits=NULL, 
+                            date.fmt="%m/%d/%Y") {
 
   parameters$Parameter <- make.names(parameters$Parameter)
   par.names <- parameters$Parameter
@@ -9,16 +10,17 @@
 
   raw.data$Site_id <- as.factor(raw.data$Site_id)
   raw.data$Site_name <- as.factor(raw.data$Site_name)
-  raw.data$Date <- as.Date(raw.data$Date, format="%m/%d/%Y")
+  raw.data$Date <- as.Date(raw.data$Date, format=date.fmt)
   raw.data <- raw.data[!is.na(raw.data$Date), ]
   
-  hm <- as.integer(raw.data$Time)
-  is.missing <- is.na(hm)
-  hm <- formatC(hm, width=4, format="d", flag="0")
-  raw.data$Time <- paste(substr(hm, 1, 2), substr(hm, 3, 4), sep=":")
-  raw.data$Time[is.missing] <- NA
+  hr.min <- as.integer(raw.data$Time)
+  is.na.time <- is.na(hr.min)
+  hr.min <- formatC(hr.min, width=4, format="d", flag="0")
+  hr.min <- paste(substr(hr.min, 1, 2), substr(hr.min, 3, 4), "00", sep=":")
+  hr.min[is.na.time] <- NA
+  raw.data$Time <- hr.min
 
-  detection.limits$Date <- as.Date(detection.limits$Date, format="%m/%d/%Y")
+  detection.limits$Date <- as.Date(detection.limits$Date, format=date.fmt)
 
   lst <- list()
   for (i in seq_along(par.names)) {
@@ -57,34 +59,36 @@
       d$dl <- NA
     }
 
-    is.event <- !d$code %in% "<" & (is.na(d$sd) | is.na(d$dl))
+    is.left <- d$code %in% "<"
+    is.event <- !is.left & (is.na(d$sd) | is.na(d$dl))
     d$t1[is.event] <- d$conc[is.event]
     d$t2[is.event] <- d$conc[is.event]
-
-    is.left <- d$code %in% "<"
-    d$t2[is.left] <- d$conc[is.left]
+    d$t2[is.left]  <- d$conc[is.left]
     lower.conc <- d$conc - 3 * d$sd
     upper.conc <- d$conc + 3 * d$sd
 
-    left <- which(!is.event & upper.conc <= d$dl)
-    d$t2[left] <- d$dl[left]
-    left <- which(!is.event & lower.conc <= d$dl & upper.conc > d$dl)
-    d$t2[left] <- upper.conc[left]
-    interval <- which(!is.event & lower.conc > d$dl)
-    d$t1[interval] <- lower.conc[interval]
-    d$t2[interval] <- upper.conc[interval]
+    are.left <- which(!is.event & upper.conc <= d$dl)
+    d$t2[are.left] <- d$dl[are.left]
+    are.left <- which(!is.event & lower.conc <= d$dl & upper.conc > d$dl)
+    d$t2[are.left] <- upper.conc[are.left]
+
+    are.interval <- which(!is.event & lower.conc > d$dl)
+    d$t1[are.interval] <- lower.conc[are.interval]
+    d$t2[are.interval] <- upper.conc[are.interval]
 
     d$t2[which(d$t2 < 0)] <- 0
     d$t1[which(d$t1 < 0 | d$t2 == 0)] <- 0
 
-    d$is.event    <- !is.na(d$t1) & !is.na(d$t2) & d$t1 == d$t2
-    d$is.left     <-  is.na(d$t1) & !is.na(d$t2)
-    d$is.interval <- !is.na(d$t1) & !is.na(d$t2) & d$t1 != d$t2
+    is.t1 <- !is.na(d$t1)
+    is.t2 <- !is.na(d$t2)
+    d$is.event    <-  is.t1 & is.t2 & d$t1 == d$t2
+    d$is.left     <- !is.t1 & is.t2
+    d$is.interval <-  is.t1 & is.t2 & d$t1 != d$t2
 
-    d <- d[order(d$Site_name, d$Date), ]
+    d <- d[order(d$Site_name, d$Date, d$Time), ]
 
-    added.attr <- parameters[match(nam, parameters$Parameter), , drop=TRUE]
-    attributes(d) <- c(attributes(d), added.attr)
+    attrs <- parameters[match(nam, parameters$Parameter), , drop=TRUE]
+    attributes(d) <- c(attributes(d), attrs)
 
     lst[[nam]] <- d
   }
@@ -99,23 +103,18 @@
 
 
   path.in <- system.file("extdata", "SIR2014", package = "Trends")
+  read.args <- list(header = TRUE, sep = "\t", na.strings = "", fill = TRUE, 
+                    strip.white = TRUE, comment.char = "", flush = TRUE, 
+                    stringsAsFactors = FALSE)
 
   file <- file.path(path.in, "Raw_Data.tsv")
-  raw.data <- read.table(file, header = TRUE, sep = "\t", na.strings = "", colClasses = "character", fill = TRUE, 
-                         strip.white = TRUE, comment.char = "", flush = TRUE, stringsAsFactors = FALSE)
-
-
-
+  raw.data <- do.call(read.table, c(list(file, colClasses = "character"), read.args))
 
   file <- file.path(path.in, "Parameters.tsv")
-  parameters <- read.table(file, header = TRUE, sep = "\t", na.strings = "", fill = TRUE, 
-                           strip.white = TRUE, comment.char = "", flush = TRUE, stringsAsFactors = FALSE)
-
-
+  parameters <- do.call(read.table, c(list(file), read.args))
 
   file <- file.path(path.in, "Detection_Limits.tsv")
-  detection.limits <- read.table(file, header = TRUE, sep = "\t", na.strings = "", fill = TRUE, 
-                                 strip.white = TRUE, comment.char = "", flush = TRUE, stringsAsFactors = FALSE)
+  detection.limits <- do.call(read.table, c(list(file), read.args))
 
   processed.data <- .ProcessRawData(raw.data, parameters, detection.limits)
 
