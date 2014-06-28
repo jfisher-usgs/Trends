22feb064b5daac7945fb2865bf5e2fada176c243
Jason C. Fisher <jfisher@usgs.gov>
2014-06-27 20:27:29 -0600

    Minor changes

--- R/TMP.R
+++ R/TMP.R
@@ -418,7 +418,7 @@
        xlab="Date", ylab=ylab, type="n", main=main, frame.plot=FALSE)
 
   if (missing(model)) {
-    cols <- c("#A80000", "#7FAF1B", "#050505", "#107FC9", "#543511")
+    cols <- c("#A80000", "#7FAF1B", "#107FC9", "#543511", "#050505")
     obj$col <- cols[as.integer(factor(obj$Name, levels=unique(obj$Name)))]
   } else {
     x <- seq(xlim[1], xlim[2], "days")
@@ -430,8 +430,6 @@
 
   lwd <- 0.5 * (96 / (6 * 12))
   tcl <- 0.50 / (6 * par("csi"))
-  pt.cex <- 0.7
-  pch <- 21
 
   is.int <- obj$is.left | obj$is.interval
   if (any(is.int)) {
@@ -441,7 +439,7 @@
   }
   if (any(obj$is.exact)) {
     o <- obj[obj$is.exact, ]
-    points(x=o$Date, y=o$t2, pch=pch, col=o$col, bg=o$col, cex=pt.cex, lwd=lwd)
+    points(x=o$Date, y=o$t2, pch=20, col=o$col)
   }
 
   at <- pretty(xlim, n=10)
@@ -455,8 +453,7 @@
   inset <- c(0.02, 0.02 * do.call("/", as.list(par("pin"))))
   if (missing(model)) {
     o <- obj[!duplicated(obj$Name), ]
-    suppressWarnings(legend("topleft", o$Name, pch=pch, col=o$col,
-                            pt.bg=o$col, pt.cex=pt.cex, pt.lwd=lwd, xpd=NA,
+    suppressWarnings(legend("topleft", o$Name, fill=o$col, border=NA, xpd=NA,
                             bg="#FFFFFFBB", box.lwd=lwd, inset=inset))
   } else {
     p <- .GetModelInfo(model)["p"]
